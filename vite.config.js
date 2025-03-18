9e925e440a82f76eb33122608c4ecc69095f5d01
AbhikThosan <abhiknaththosan@gmail.com>
2025-03-19 02:51:13 +0600

    fix/deploy-fix-vite-config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.VITE_BASE_PATH || "/hands-on-fe",
+  // base: process.VITE_BASE_PATH || "/hands-on-fe",
 });
